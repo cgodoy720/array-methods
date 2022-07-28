59baf8596db9853ab2462b6e036e26db40f8b118
Carlos Godoy <carlosgodoy@pursuit.org>
2022-07-28 00:24:07 -0400

    adding starter code

---
+++ index.js
@@ -0,0 +1,55 @@
+const characters = [
+    {
+        name: "Daenerys Targaryen",
+        show: "Game of Thrones",
+        seasons: 8
+    },
+    {
+        name: "Dolores Abarnathy",
+        show: "West World",
+        seasons: 4
+    },
+    {
+        name: "Don Draper",
+        show: "Mad Men",
+        seasons: 7
+    },
+    {
+        name: "Thomas Shelby",
+        show: "Peaky Blinders",
+        seasons: 6
+    },
+    {
+        name: "Rick Sanchez",
+        show: "Rick & Morty",
+        seasons: 4
+    },
+    {
+        name: "Jeff Probst",
+        show: "Survivor",
+        seasons: 43
+    },
+    {
+        name: "Charlotte Hale",
+        show: "West World",
+        seasons: 4
+    }
+]
+
+
+// forEach()
+
+
+// map()
+
+
+// filter()
+
+
+// every()
+
+
+// some()
+
+
+// find()
